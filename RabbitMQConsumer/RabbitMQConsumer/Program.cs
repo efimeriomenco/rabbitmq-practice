bc6a0d8b24a312e939cbc312bdb7ed26b6224c73
efimeriomenco <efim.eriomenco@outlook.com>
2022-04-29 11:27:01 +0300

    First commit

---
+++ RabbitMQConsumer/RabbitMQConsumer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQConsumer;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://guest:guest@localhost:5672")
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+QueueConsumer.Consume(channel);
